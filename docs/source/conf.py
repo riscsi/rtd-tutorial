7d10864422b3471ca349fd98668b160757ba5756
riscsi <mail@risc.si>
2022-02-21 23:30:33 +0100

    Getting started

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Unofficial Veritak Tutorial'
+copyright = '2022, No copyright'
+author = 'Some Guy'
 
 release = '0.1'
 version = '0.1.0'
